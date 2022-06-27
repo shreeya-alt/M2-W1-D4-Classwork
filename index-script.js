80923a5d995d151a152403c0ef0d2d1496b01d62
shreeya-alt <shreeyamaskey.1@gmail.com>
2022-06-27 22:40:47 +0545

    This is CW for M2 W1 D4

---
+++ index-script.js
@@ -0,0 +1,14 @@
+/* var element_header = document.createElement('header');
+var element_h1 = document.createElement('h1');
+var text_h1 = document.createTextNode("This is content for H1 tag");
+var test = document.querySelector('#test');
+element_h1.appendChild(text_h1);
+element_header.appendChild(element_h1);
+document.body.appendChild(element_header);
+test.appendChild(element_h1)
+test.classList.add('class1'); */
+
+function function_1() {
+    
+}
+
